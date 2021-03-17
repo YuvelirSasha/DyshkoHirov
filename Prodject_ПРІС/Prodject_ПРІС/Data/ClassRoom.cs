90c6c5b9f62408117f15d2212e939d898249ff31
Sasha Hirov <75026042+YuvelirSasha@users.noreply.github.com>
2021-03-18 00:05:57 +0200

    add models + en fr
    создав моделі в папці Data і добавив бібліотекі en

---
+++ Prodject_ПРІС/Prodject_ПРІС/Data/ClassRoom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodject_ПРІС.Data
+{
+    public class ClassRoom
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idCr { get; set; }
+
+        [StringLength(255)]
+        public string nameCr { get; set; }
+
+        public DateTime DateCr { get; set; }
+    }
+}
---
+++ Prodject_ПРІС/Prodject_ПРІС/Data/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodject_ПРІС.Data
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idst { get; set; }
+
+        [StringLength(255)]
+        public string nameSt { get; set; }
+
+        public DateTime birthadateSt { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        public DateTime DateSt { get; set; }
+        
+        [ForeignKey("ClassRoom")]
+        public int IdCr { get; set; }
+
+    }
+}
