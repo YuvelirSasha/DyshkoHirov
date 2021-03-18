489bcd4366f70ea131601a1ca5845d75c29055bf
Sasha Hirov <75026042+YuvelirSasha@users.noreply.github.com>
2021-03-18 12:46:27 +0200

    create BD
    створив міграцію і підключив моделі до бази даних

---
+++ Prodject_ПРІС/Prodject_ПРІС/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Prodject_ПРІС.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<ClassRoom> ClassRooms { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
