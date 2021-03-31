06f748739b3758e836b0a0ffcdd669db15df3092
Sasha Hirov <75026042+YuvelirSasha@users.noreply.github.com>
2021-03-31 18:47:16 +0300

    переробив проект/ add login and register

---
+++ DemoMVC/Context/SchoolDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoMVC.Models;
+
+namespace DemoMVC.Context
+{
+    public class SchoolDatabaseContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<ClassRoom> ClassRooms { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public SchoolDatabaseContext(DbContextOptions<SchoolDatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        
+    }
+}
---
+++ DemoMVC/Migrations/20210324190014_konets.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DemoMVC.Migrations
+{
+    public partial class konets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClassRooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Other = table.Column<byte>(type: "tinyint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClassRooms", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClassRooms");
+        }
+    }
+}
